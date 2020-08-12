992299bc20f931f838778659b8805b380d72b7cc
Dhanamjaya <dhanamjaya.k567@gmail.com>
2020-08-12 10:48:06 +0530

    intial commit

---
+++ src/main/java/com/app/ControllerbasedExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerbasedExceptionHandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerbasedExceptionHandlingApplication.class, args);
+	}
+
+}
